1b0d071764ea83e934ce0e1309f2be3462d2423c
전형탁 <andy395023@gmail.com>
2017-11-27 16:06:08 +0900

    Add files via upload

--- test_event/call_timer.js
+++ test_event/call_timer.js
@@ -1,3 +1,8 @@
 var module = require('./custom_module_timer');
 
-// module
+// module내부에 선언된 timer객체를 통해 tick이벤트를 캐치하고,
+// 이벤트 발생시마다 현재시간 출력
+module.timer.on('tick', function(time){
+    var time = new Date();
+    console.log('now :'+time);
+});
